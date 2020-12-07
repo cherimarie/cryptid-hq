c9b703ad8cc6df55b4f620551301b5b886b7ab14
CheriMarie <cherimarie@gmail.com>
2020-12-06 17:57:05 -0800

    Pages & header component

---
+++ components/CustomHeader.vue
@@ -0,0 +1,40 @@
+<template>
+<header>
+    <h1>{{headline}}</h1>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">HQ</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/elves">ELVES</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/bigfeet">BIGFEET</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/mermaids">MERMAIDS</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'CustomHeader',
+    props: ['headline']
+  }
+</script>
+
+<style lang="scss">
+h1 {color: magenta;}
+ul {
+  list-style: none;
+  padding: .5rem;
+  background-color: violet;
+}
+li {
+  display: inline-block;
+}
+</style>
---
+++ pages/mermaids.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <CustomHeader headline="MERMAIDS" />
+  </div>
+</template>
+
+<script type="text/javascript"></script>
+
+<style lang="scss"></style>
